fb5809b6eb9cecf83348b27c3d2d6384ea6fa418
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2025-06-08 19:08:15 +0200

    fix img

--- src/components/GallerySection.jsx
+++ src/components/GallerySection.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import img1 from '../assets/img/Copia di Testo del paragraf.png';
-import img2 from '../assets/img/Copia di Testo del paragrafo.png';
-import img3 from '../assets/img/DJSCOVERY LOGO.png';
-import img4 from '../assets/img/LOGO PRINCIPALE.png';
-import img5 from '../assets/img/Testo del paragrafo_HQ.png';
-import img6 from '../assets/img/Testo del paragrafo_HQ2.png';
-import img7 from '../assets/img/hero.png';
-import img8 from '../assets/img/logo-dj.png';
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import img1 from "../assets/img/Copia di Testo del paragraf.png";
+import img2 from "../assets/img/Copia di Testo del paragrafo.png";
+import img3 from "../assets/img/DJSCOVERY LOGO.png";
+import img4 from "../assets/img/LOGO PRINCIPALE.png";
+import img5 from "../assets/img/Testo del paragrafo_HQ.png";
+import img6 from "../assets/img/Testo del paragrafo_HQ2.png";
+import img7 from "../assets/img/hero.png";
+import img8 from "../assets/img/logo-dj.png";
 
 const Section = styled.section`
   padding: 2rem 0;
@@ -36,22 +36,7 @@ const StyledSwiper = styled(Swiper)`
   }
 `;
 
-const images = [
-  'https://source.unsplash.com/400x300/?music',
-  'https://source.unsplash.com/400x300/?party',
-  'https://source.unsplash.com/400x300/?dj',
-  'https://source.unsplash.com/400x300/?concert',
-  'https://source.unsplash.com/400x300/?club',
-  'https://source.unsplash.com/400x300/?festival',
-  img1,
-  img2,
-  img3,
-  img4,
-  img5,
-  img6,
-  img7,
-  img8
-];
+const images = [img1, img2, img3, img4, img5, img6, img7, img8];
 
 const GallerySection = () => (
   <Section>
@@ -65,7 +50,7 @@ const GallerySection = () => (
         slidesPerView={1}
         breakpoints={{
           640: { slidesPerView: 2 },
-          1024: { slidesPerView: 3 }
+          1024: { slidesPerView: 3 },
         }}
       >
         {images.map((img, idx) => (
